48370ab2f19d3370de0c0c55fba7f2c56179d71b
Masashi Tsuji <m.tsuji1119@gmail.com>
2013-05-30 17:01:59 +0900

    Update SetupManager.js

--- SetupManager.js
+++ SetupManager.js
@@ -5,10 +5,10 @@ function require(option) {
 	document.write(script);
 }
 
-require({src: 'https://raw.github.com/jquery/jquery-tmpl/master/jquery.tmpl.js'});
+require({src: 'https://rawgithub.com/jquery/jquery-tmpl/master/jquery.tmpl.js'});
 require({src: 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js'});
-require({src: 'https://raw.github.com/htz/Salesforce.com-Setup-Manager/master/jquery.cookie.js'});
-require({src: 'https://raw.github.com/htz/Salesforce.com-Setup-Manager/master/jquery.json.min.js'});
+require({src: 'https://rawgithub.com/htz/Salesforce.com-Setup-Manager/master/jquery.cookie.js'});
+require({src: 'https://rawgithub.com/htz/Salesforce.com-Setup-Manager/master/jquery.json.min.js'});
 
 (function($) {
 	$(function() {
